52d560309cc41bf6a009796ebc1f44113048d1ea
yaoxin-sudo <yx17693121776@163.com>
2020-04-10 16:43:18 +0800

    修改

---
+++ YaoXin.java
@@ -0,0 +1,23 @@
+package xin.yao;
+
+public class YaoXin {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		String name = "姚鑫";
+		int age = 25;
+		String hoppy = "play basketball";
+		double weight = 85.5;
+		char sex = '男';
+		System.out.println("自我介绍");
+		System.out.println("---------");
+		System.out.println("姓名:"+name);
+		System.out.println("年龄:"+age+"岁");
+		System.out.println("爱好:"+hoppy);
+		System.out.println("体重:"+weight+"kg");
+		System.out.println("性别:"+sex);
+		
+
+	}
+
+}
